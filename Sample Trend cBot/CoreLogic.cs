02595bc42a82df02b85a004229f91a2c4fc9d05c
Nick Gilbert <nickgilbert990@gmail.com>
2019-01-08 00:47:44 +0000

    Abstracting API calls into helper classes to support unit testing (mocking)

--- Sample Trend cBot/CoreLogic.cs
+++ Sample Trend cBot/CoreLogic.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using cAlgo;
 using cAlgo.API;
 using cAlgo.API.Indicators;
@@ -34,14 +35,14 @@ namespace cAlgo
         [Parameter("Stop Loss", DefaultValue = 30)]
         public int StopLoss { get; set; }
 
-        private const string Label = "FxAutomation";
+        public string Label = "FxAutomation";
         private  IIndicators _indicator;
+        private GetOpenPositions _getOpenPositions;
 
         ///<summary>
         /// Parameters to be passed into the indicator factory
         /// </summary>
-
-        public class FactoryParameters
+        public struct FactoryParameters
         {
             public string IndicatorType;
             public MovingAverageType MAType;
@@ -56,13 +57,22 @@ namespace cAlgo
             ///<summary>
             /// Initialsise parameters from input data and pass to the factory which returns the object identified by IndicatorType
             /// </summary>
-
             var factoryParameters = new FactoryParameters {Bot = this, IndicatorType = "MA", MAType = MAType, FastPeriods = FastPeriods, SlowPeriods = SlowPeriods, SourceSeries = SourceSeries};
             _indicator = new IndicatorFactory().GetIndicator(factoryParameters);
+
+            ///<summary>
+            /// Instanciate positions object to abstract API functions from the main logic
+            /// </summary>
+            _getOpenPositions = new GetOpenPositions(this);
         }
 
         protected override void OnTick()
         {
+            ///<summary>
+            /// Get open positions
+            /// </summary>
+            var longPositions = _getOpenPositions.LongPositions();
+
             var longPosition = Positions.Find(Label, Symbol, TradeType.Buy);
             var shortPosition = Positions.Find(Label, Symbol, TradeType.Sell);
 
@@ -84,6 +94,31 @@ namespace cAlgo
         {
             get { return Symbol.QuantityToVolume(Quantity); }
         }
+    }
+
+    ///<summary>
+    /// Get open positions from the cAlgo API passing the instance of the main class into the constructor.
+    /// The purpose of this helper class is to abstract the API calls from the main logic.
+    /// </summary>
+    public class GetOpenPositions 
+    {
+        private SampleTrendcBot _bot;
+
+        public GetOpenPositions(SampleTrendcBot bot)
+        {
+            _bot = bot;
+        }
+
+        public Position LongPositions()
+        {
+            return _bot.Positions.Find(_bot.Label, _bot.Symbol, TradeType.Buy);
+        }
+
+        public Position ShortPositions()
+        {
+            return _bot.Positions.Find(_bot.Label, _bot.Symbol, TradeType.Sell);
+        }
+
     }
 }
 
